de23dd7e1b3f9ef912627aee953ba0e323d916e2
aiggo <aiko.benjamins@padis.io>
2022-12-03 22:40:23 +0100

    Added a lot of stuff

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,35 @@
 import * as THREE from "three";
+import { TWEEN } from "three/examples/jsm/libs/tween.module.min";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { createFloorTile } from "./sceneobjects/createFloorTile";
 import "./style.css";
+import { addCollisionElements } from "./sceneobjects/addCollisionElements";
+import { Box3, BoxGeometry, Mesh, MeshLambertMaterial, Vector3 } from "three";
+import { createHearts } from "./sceneobjects/createHearts";
+
+const boatElement = "resources/models/fishing_boat/scene.gltf";
+const skyBoxTexture = "resources/textures/skyboxes/darkcartoon.jpeg";
+const gltfLoader = new GLTFLoader();
+
+const debug = false;
+
+/** Movement */
+const startSpeed = 0.5;
+const maxSpeed = 20;
+let movementSpeed = 10;
+let acceleration = 0.05;
+let moving = false;
+
+/** Spawning */
+let spawnDelay = 1.2;
+const clock = new THREE.Clock(true);
+let lastSpawn = 0;
+
+/** collision */
+let enemyCollidersArray: THREE.Sphere[] = [];
+let lives = 3;
+let collisionDebouncer = false;
 
 const scene = new THREE.Scene();
 
@@ -12,103 +40,283 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+/** Renderer */
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg") || undefined,
+  antialias: true,
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-
-const threeTone = new THREE.TextureLoader().load(
-  "resources/gradientMaps/threeTone.jpg"
-);
-threeTone.minFilter = THREE.NearestFilter;
-threeTone.magFilter = THREE.NearestFilter;
-
-camera.position.setZ(30);
-camera.position.setX(-3);
+const cameraOffset = new Vector3(-0.05, 7, 13); // NOTE Constant offset between the camera and the target
+camera.position.set(cameraOffset.x, cameraOffset.y, cameraOffset.z);
+scene.add(camera);
 
 /** HELPERS */
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 const gridHelper = new THREE.GridHelper(200, 50);
 const axesHelper = new THREE.AxesHelper(5);
-scene.add(gridHelper, axesHelper);
+if (debug) scene.add(gridHelper, axesHelper);
 
 /** LIGHTING */
-const pointLight = new THREE.PointLight(0xffffff, 1);
-const pointLightHelper = new THREE.PointLightHelper(pointLight);
-pointLight.position.set(10, 10, 10);
+const directionalLight = new THREE.DirectionalLight("#ffeac0", 0.9);
+const helper = new THREE.DirectionalLightHelper(directionalLight, 5);
+directionalLight.castShadow = true;
+directionalLight.position.set(5, 10, 3);
+directionalLight.rotation.set(0.5, 0, -0.5);
+scene.add(directionalLight);
+if (debug) scene.add(helper);
 
-// const ambientLight = new THREE.AmbientLight(0xffffff, 0);
-scene.add(pointLight, pointLightHelper);
+const ambientLight = new THREE.AmbientLight("#ffffff", 0.2);
+scene.add(ambientLight);
 
-/** GEOMETRY */
-const torusGeo = new THREE.TorusKnotGeometry(10, 3, 10, 100);
-const torusMaterial = new THREE.MeshToonMaterial({
-  color: 0xff0110,
-  wireframe: false,
-  side: THREE.FrontSide,
+/** SETUP floor */
+scene.add(createFloorTile());
+
+/** Setup Skybox*/
+
+const loader = new THREE.TextureLoader();
+const texture = loader.load(skyBoxTexture, () => {
+  const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
+  rt.fromEquirectangularTexture(renderer, texture);
+  scene.background = rt.texture;
 });
 
-const torusMesh = new THREE.Mesh(torusGeo, torusMaterial);
+/** Create Boat model */
+const boatCube = new Mesh(
+  new BoxGeometry(2.3, 5, 6),
+  new MeshLambertMaterial({ wireframe: debug, visible: false })
+);
 
-scene.add(torusMesh);
+const boatBoundingBox = new Box3(new Vector3(), new Vector3());
+boatBoundingBox.setFromObject(boatCube);
+let boatModel; // gltf
+let boatGroup = new THREE.Group();
+boatGroup.add(boatCube);
+gltfLoader.load(
+  boatElement,
+  (gltf) => {
+    boatModel = gltf.scene;
+    boatModel.rotation.y = 3.15;
+    boatModel.scale.set(0.01, 0.01, 0.01);
 
-let increase = true;
-let tubularSegmentIterator = 10;
+    console.log("boardBoundingBox", boatBoundingBox);
 
-const setupKeyControls = () => {
-  // var cube = scene.getObjectByName("cube");
-  document.onkeydown = function (e) {
-    console.log("e.code", e.code);
-    switch (e.code) {
-      case "ArrowLeft":
-        torusMesh.position.z += 0.1;
-        break;
-      case "ArrowUp":
-        torusMesh.position.x -= 0.1;
-        break;
-      case "ArrowRight":
-        torusMesh.position.z -= 0.1;
-        break;
-      case "ArrowDown":
-        torusMesh.position.x += 0.1;
-        break;
-    }
-  };
+    boatGroup.add(boatModel);
+  },
+  undefined,
+  (error) => {
+    console.error(error);
+  }
+);
+scene.add(boatGroup);
+
+/** Boat light */
+const pointLight = new THREE.PointLight("#ffffff", 0.8);
+const pointLightHelper = new THREE.PointLightHelper(pointLight);
+pointLight.position.set(0, 5, 14);
+boatGroup.add(pointLight);
+scene.add(pointLightHelper);
+
+/** Heart model */
+
+let heartModel1;
+let heartModel2;
+let heartModel3;
+
+createHearts(0, (heartModelNew) => {
+  heartModel1 = heartModelNew;
+  camera.add(heartModel1);
+});
+
+createHearts(1.5, (heartModelNew) => {
+  heartModel2 = heartModelNew;
+  camera.add(heartModel2);
+});
+
+createHearts(3, (heartModelNew) => {
+  heartModel3 = heartModelNew;
+  camera.add(heartModel3);
+});
+
+const movementHandler = (evt: KeyboardEvent) => {
+  switch (evt.key) {
+    case "s":
+      moveBack();
+      break;
+    case "w":
+      moving = true;
+      moveForward();
+      break;
+    case "q":
+      rotLeft();
+      break;
+    case "e":
+      rotRight();
+      break;
+    case "a":
+      moveLeft();
+      break;
+    case "d":
+      moveRight();
+      break;
+    case "f":
+      break;
+  }
 };
 
-setupKeyControls();
+const updateCamera = () => {
+  const objectPosition = new Vector3();
+  boatGroup.getWorldPosition(objectPosition);
 
+  console.log("objectPosition", objectPosition);
+  camera.position.copy(objectPosition).add(cameraOffset);
+};
+
+const moveWorldForward = () => {
+  boatGroup.position.z -= movementSpeed;
+  camera.position.z -= movementSpeed;
+  controls.target.copy(boatGroup.position);
+};
+
+const moveForward = () => {
+  const newPosZ = boatGroup.position.z - movementSpeed;
+  new TWEEN.Tween(boatGroup.position)
+    .to({ z: newPosZ }, 1000)
+    .easing(TWEEN.Easing.Quadratic.Out)
+    .onUpdate((object) => {
+      boatGroup.position.z = object.z;
+      camera.position.z = object.z + cameraOffset.z;
+      controls.target.copy(object);
+    })
+    .start();
+};
+
+const moveBack = () => {
+  boatGroup.position.z += movementSpeed;
+  camera.position.z += movementSpeed;
+  controls.target.copy(boatGroup.position);
+};
+
+const moveLeft = () => {
+  const newPosX = boatGroup.position.x - movementSpeed * 10;
+  new TWEEN.Tween(boatGroup.position)
+    .to({ x: newPosX }, 1000)
+    .easing(TWEEN.Easing.Quadratic.Out)
+    .onUpdate((object) => {
+      boatGroup.position.x = object.x;
+      camera.position.x = object.x + cameraOffset.x;
+      controls.target.copy(object);
+    })
+    .start();
+};
+
+const moveRight = () => {
+  const newPosX = boatGroup.position.x + movementSpeed * 10;
+  new TWEEN.Tween(boatGroup.position)
+    .to({ x: newPosX }, 1000)
+    .easing(TWEEN.Easing.Quadratic.Out)
+    .onUpdate((object) => {
+      boatGroup.position.x = object.x;
+      camera.position.x = object.x + cameraOffset.x;
+      controls.target.copy(object);
+    })
+    .start();
+};
+const rotLeft = () => {
+  boatGroup.rotation.y += Math.PI / 60;
+};
+
+const rotRight = () => {
+  boatGroup.rotation.y -= Math.PI / 60;
+};
+
+const accelerateMovementSpeed = () => {
+  if (moving) {
+    if (movementSpeed <= maxSpeed) {
+      movementSpeed += acceleration;
+    }
+  } else {
+    movementSpeed = startSpeed;
+  }
+};
+
+const spawnEnemies = () => {
+  const elapsed = clock.getElapsedTime();
+  if (elapsed - spawnDelay > lastSpawn) {
+    console.log("spawn");
+
+    const collisionElements = addCollisionElements(boatGroup);
+    scene.add(collisionElements.collisionElements);
+    enemyCollidersArray = [
+      ...enemyCollidersArray,
+      ...collisionElements.colliders,
+    ];
+    lastSpawn = elapsed;
+
+    if (spawnDelay > 0.1) {
+      spawnDelay -= 0.01;
+    }
+  }
+};
+
+const checkCollision = () => {
+  const foundCollision = enemyCollidersArray.find((enemy) => {
+    return enemy.intersectsBox(boatBoundingBox);
+  });
+
+  if (foundCollision) {
+    if (!collisionDebouncer) {
+      console.log("ALARM");
+      collisionDebouncer = true;
+      switch (lives) {
+        case 3:
+          camera.remove(heartModel3);
+          break;
+        case 2:
+          camera.remove(heartModel2);
+          break;
+        case 1:
+          camera.remove(heartModel1);
+          break;
+      }
+
+      lives -= 1;
+    }
+  } else {
+    collisionDebouncer = false;
+  }
+};
+
+const init = () => {
+  clock.start();
+  window.addEventListener("keydown", movementHandler, false);
+  window.addEventListener(
+    "keyup",
+    () => {
+      moving = false;
+    },
+    false
+  );
+};
+console.log("boatModel", boatModel);
 const animate = () => {
   requestAnimationFrame(animate);
 
-  // torusMesh.rotation.x += 0.01;
-  // torusMesh.rotation.y += 0.001;
-  // torusMesh.rotation.z += 0.01;
+  boatBoundingBox
+    .copy(boatCube.geometry.boundingBox)
+    .applyMatrix4(boatCube.matrixWorld);
 
-  if (tubularSegmentIterator >= 128) {
-    increase = false;
-  } else if (tubularSegmentIterator <= 5) {
-    increase = true;
-  }
-  torusMaterial.gradientMap = threeTone;
+  spawnEnemies();
+  checkCollision();
+  moveWorldForward();
+  TWEEN.update();
 
-  if (increase) {
-    tubularSegmentIterator += 0.1;
-  } else {
-    tubularSegmentIterator -= 0.1;
-  }
-
-  torusMesh.geometry = new THREE.TorusKnotGeometry(
-    10,
-    3,
-    tubularSegmentIterator,
-    100
-  );
-
+  accelerateMovementSpeed();
   controls.update();
   renderer.render(scene, camera);
 };
 
+init();
 animate();
---
+++ src/sceneobjects/createFloorTile.ts
@@ -0,0 +1,105 @@
+import {
+  Mesh,
+  MeshStandardMaterial,
+  PlaneGeometry,
+  RepeatWrapping,
+  ShadowMaterial,
+  TextureLoader,
+} from "three";
+
+const waterTexture = "resources/textures/Water_001_SD/Water_001_COLOR.jpg";
+const waterDisplacementMap =
+  "resources/textures/Water_001_SD/Water_001_DISP.png";
+
+const createFloorTile = () => {
+  let texture = new TextureLoader().load(waterTexture);
+  texture.repeat.set(300, 300);
+  texture.wrapT = RepeatWrapping;
+  texture.wrapS = RepeatWrapping;
+
+  let planeGeometry = new PlaneGeometry(10000, 10000);
+  let material = new MeshStandardMaterial({
+    map: texture,
+  });
+
+  //material.displacementMap = waterTextureDISP;
+  //material.displacementScale = 10;
+  //material.displacementBias = 1;
+
+  let mesh = new Mesh(planeGeometry, material);
+
+  mesh.position.set(0, -0.1, 0);
+  mesh.rotation.set(Math.PI / -2, 0, 0);
+
+  return mesh;
+};
+
+const createFloorShadow = () => {
+  const geometry = new PlaneGeometry(2000, 2000);
+  geometry.rotateX(-Math.PI / 2);
+
+  const material = new ShadowMaterial();
+  material.opacity = 0.2;
+
+  const plane = new Mesh(geometry, material);
+  plane.position.y = 0;
+  plane.receiveShadow = true;
+  return plane;
+};
+
+/** Currently not used/broken */
+const SpriteSheetTexture = (
+  imageURL: string,
+  framesX: number,
+  framesY: number,
+  frameDelay: number,
+  _endFrame: number
+) => {
+  let frameWidth: number = 0;
+  let frameHeight: number = 0;
+  let x = 0;
+  let y = 0;
+  let count = 0;
+  const endFrame = _endFrame || framesX * framesY;
+  const canvas = document.createElement("canvas");
+  let ctx = canvas.getContext("2d");
+  const canvasTexture = new CanvasTexture(canvas);
+  const img = new Image();
+
+  img.onload = () => {
+    frameWidth = img.width / framesX;
+    frameHeight = img.height / framesY;
+    canvas.width = frameWidth;
+    canvas.height = frameHeight;
+    setInterval(() => {
+      count++;
+
+      if (count >= endFrame) {
+        count = 0;
+      }
+
+      x = (count % framesX) * frameWidth;
+      y = ((count / framesX) | 0) * frameHeight;
+
+      ctx.clearRect(0, 0, frameWidth, frameHeight);
+      ctx.drawImage(
+        img,
+        x,
+        y,
+        frameWidth,
+        frameHeight,
+        0,
+        0,
+        frameWidth,
+        frameHeight
+      );
+
+      canvasTexture.needsUpdate = true;
+    }, frameDelay);
+  };
+  img.src = imageURL;
+
+  return canvasTexture;
+};
+
+export { createFloorTile, SpriteSheetTexture, createFloorShadow };
